29b7b09e418a89e468bd7858c0ff7c1cdb949e46
Mark Suzuki <markdsuzuki@gmail.com>
2018-07-14 12:28:24 -0700

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   let arr = [0,1]
+//   if(n === 1 || n === 0) return n 
+//   for(let i = 2; i <=n; i++){
+//     arr.push(arr[i-1] + arr[i-2])
+//   }
+//   return arr[n]
+// }
 
+// function fib(n) {
+//   if(n <= 1) return n 
+//   return fib(n-1) + fib(n-2)
+// }
+
+// Terrible Time Complexity Because of O(log(2^n)) EXPONENTIAL
+
+// IMPORVE RECURSIVE SOLUTION - MEMOZIATION
+
+function fib(n, memo = []) {
+  if(n <= 1) return n 
+  if(memo[n]) return memo[n]
+  memo[n] = fib(n-1, memo) + fib(n-2, memo)
+  return memo[n]
+}
 module.exports = fib;
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let start = 1
+  let startRow = 0
+  let startCol = 0
+  let endRow = n - 1 // matrix length - 1 need index position
+  let endCol = n - 1 
+  // create an array 
+
+  // not working with new Array(n).fill(new Array(),n)
+  let array = []
+
+  for(let o = 0; o < n; o++){
+    array.push([])
+  }
+
+  while(startCol <= endCol && startRow <= endRow){
+    for(let i = startCol; i <= endCol; i++){
+      array[startRow][i] = start 
+      start++
+    }
+    startRow++
+
+    for(let j = startRow; j <= endRow; j++){
+      array[j][endCol] = start
+      start++
+    }
+    endCol--
+
+    for(let k = endCol; k >= startCol; k--){
+      array[endRow][k] = start 
+      start++
+    }
+    endRow--
+
+    for(let l = endRow; l >= startRow; l--){
+      array[l][startCol] = start 
+      start++
+    }
+    startCol++
+    
+  }
+  return array 
+}
 
 module.exports = matrix;
